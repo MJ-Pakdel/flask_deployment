16a06fe5e5bfc6019593452556c34905e59e857b
mj-pakdel <pakdel.mj@gmail.com>
2024-03-01 16:30:59 -0800

    abaddsdfs

--- modules/ecs-ecr/main.tf
+++ modules/ecs-ecr/main.tf
@@ -116,6 +116,7 @@ resource "aws_lb_target_group" "app_tg" {
   port     = 80
   protocol = "HTTP"
   vpc_id   = var.vpc_id
+  target_type = "ip"
   health_check {
     healthy_threshold   = 2
     unhealthy_threshold = 2
